caec47b600a0554f932dd66a8f32c4e352758587
unknown <hudge.rhudge.rujuta0@gmail.com>
2022-10-07 13:47:00 +0530

    Angular files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'passdata-demo';
+
+  myform = new FormGroup({
+
+    name: new FormControl(''),
+    address: new FormControl(''),
+    age: new FormControl(''),
+  });
+
+  formdata: {
+
+    name: string;
+    address: string;
+    age: string;
+  }[] = [];
+
+  senddata() {
+    this.formdata.push(this.myform.value);
+    console.log(this.formdata);
+  }
 }
+
